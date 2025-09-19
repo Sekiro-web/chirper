b24ec5bd1095f0d7678ff92eb548ef2987803e60
sekiro <sekiro@gmail.com>
2025-09-19 21:18:11 +0300

    Just put the last tips

--- database/seeders/ChirpSeeder.php
+++ database/seeders/ChirpSeeder.php
@@ -8,21 +8,5 @@ use App\Models\Chirp;
 
 class ChirpSeeder extends Seeder
 {
-    public function run(): void
-    {
-        $names = ['Sheko', 'Fawzy', 'Sekiro'];
-        $messages = ['first', 'second', 'third'];
-
-        foreach (array_map(null, $names, $messages) as [$name, $message]) {
-            $user = User::factory()->create([
-                'name' => $name,
-                'email' => strtolower($name) . '@gmail.com',
-            ]);
-
-            Chirp::factory()->create([
-                'user_id' => $user->id,
-                'message' => "This is the {$message} chirp",
-            ]);
-        }
-    }
+//
 }
